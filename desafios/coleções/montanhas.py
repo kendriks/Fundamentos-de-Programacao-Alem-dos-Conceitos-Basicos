2e7b0fd4143fa25fae43cd493b07e232e6e53375
Kendriks <156589321+kendriks@users.noreply.github.com>
2026-08-25 21:14:48 +0000

    feat: adiciona desafio de lista e dicionário

---
+++ desafios/coleções/montanhas.py
@@ -0,0 +1,11 @@
+montanhas = {
+    'Africana' : 'Kilimanjaro',
+    'Antartica' : 'Macizo Vinson',
+    'Indoaustraliana' : 'Monte Jaya',
+    'Euroasiatica' : 'Everest',
+    'Norteamerica' : 'Monte Denali',
+    'Pacifico' : 'Volcán Mauna Kea',
+    'Suramericana' : 'aconcagua',
+}
+
+print(montanhas['Pacifico']);
