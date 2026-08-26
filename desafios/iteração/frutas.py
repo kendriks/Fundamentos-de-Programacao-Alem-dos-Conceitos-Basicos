e3390084360500e5790c7b43a9df7fd70b446e41
Kendriks <156589321+kendriks@users.noreply.github.com>
2026-08-26 17:29:24 +0000

    feat: adiciona desafio com for

---
+++ desafios/iteração/frutas.py
@@ -0,0 +1,12 @@
+frutas = [
+    'manzanas',
+    'bananas',
+    'peras',
+    'mangos',
+    'mandarinas',
+    'granadas',
+]
+
+print("<<Seleção de Frutas>>")
+for fruta in frutas:
+    print(fruta)
